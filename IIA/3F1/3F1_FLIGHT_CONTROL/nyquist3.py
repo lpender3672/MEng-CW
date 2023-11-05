c09834c175ee5bfe0c5c672ae61a5beb520a3040
lpender3672 <louispender3672@gmail.com>
2023-11-05 15:58:34 +0000

    progress with 3F1

--- IIA/3F1/3F1_FLIGHT_CONTROL/nyquist3.py
+++ IIA/3F1/3F1_FLIGHT_CONTROL/nyquist3.py
@@ -8,16 +8,13 @@ import matplotlib.pyplot as plt
 
 W = np.logspace(-10,10, 10000)
 
-k = 0.989
 D = 0.317
-
-M = 10
-N = 10
+T = 0.5
 
 def Gjw(w):
     s = 1j * w
-    K = k*np.exp(- s * D)
-    G = K * N / (s**2 + M*s )
+    K = np.exp(- s * D)
+    G = K* 2 / (T*s - 1)
     return G
 
 #polar plot
@@ -27,19 +24,14 @@ ax.plot(np.real(G), np.imag(G), color='blue', linewidth=1.0, label='G(s)')
 G = Gjw(-W)
 ax.plot(np.real(G), np.imag(G), color='blue', linewidth=1.0, label='G(s)', linestyle='dashed')
 
-asymtope_line = np.linspace(-1, 1, 100) * 1j - k * N * (D + 1/M) / M
-ax.plot(np.real(asymtope_line), np.imag(asymtope_line), color='red', linewidth=1.0, label='Asymptote', linestyle='dashdot')
-
-ax.set_xlim(-0.7, 0.2)
-ax.set_ylim(-0.5, 0.5)
 ax.set_aspect('equal')
 ax.grid(which='both')
 
 ax.set_xlabel('Real')
 ax.set_ylabel('Imaginary')
-ax.set_title(f'Nyquist plot of plant, $G_1(s)$ in series with gain {k} and delay {D}')
+ax.set_title(f'Nyquist plot of plant $G_2(j\omega)$ with controller delay {D}')
 
 path = "IIA/3F1/3F1_FLIGHT_CONTROL"
 
-plt.savefig(path + '/figures/nyquist1.png', dpi=300)
+plt.savefig(path + '/figures/nyquist3.png', dpi=300)
 plt.show()
