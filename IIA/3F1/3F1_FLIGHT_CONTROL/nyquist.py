cad1b49e2c2ab7abac984e93ddc1604745a29e82
lpender3672 <louispender3672@gmail.com>
2023-11-08 12:47:46 +0000

    alright i made all the graphs computer moden serif beautiful

--- IIA/3F1/3F1_FLIGHT_CONTROL/nyquist.py
+++ IIA/3F1/3F1_FLIGHT_CONTROL/nyquist.py
@@ -2,6 +2,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+plt.rcParams["text.usetex"] = True
+plt.rcParams["font.family"] = "serif"
+plt.rcParams['figure.facecolor'] = 'white'
+
 # Define the system parameters
 
 ## 2.1 SIMPLIFIED AIRCRAFT MODEL
@@ -14,6 +18,7 @@ D = 0.250
 M = 10
 N = 10
 
+
 def Gjw(w):
     s = 1j * w
     K = k*np.exp(- s * D)
@@ -24,8 +29,21 @@ def Gjw(w):
 G = Gjw(W)
 fig, ax = plt.subplots()
 ax.plot(np.real(G), np.imag(G), color='blue', linewidth=1.0, label='G(s)')
+
+# arrow
+idx = np.where(np.isclose(G.imag, -0.2, atol=0.001))[0][0]
+x,y = G[idx].real, G[idx].imag
+dx,dy = x - G[idx+1].real, y - G[idx+1].imag
+ax.arrow(x, y, dx, dy, width=0.005, color = 'blue')
+
 G = Gjw(-W)
 ax.plot(np.real(G), np.imag(G), color='blue', linewidth=1.0, label='G(s)', linestyle='dashed')
+
+# -w arrow
+idx = np.where(np.isclose(G.imag, 0.2, atol=0.001))[0][0]
+x,y = G[idx].real, G[idx].imag
+dx,dy = x - G[idx-1].real, y - G[idx-1].imag
+ax.arrow(x, y, dx, dy, width=0.005, color = 'blue')
 
 asymtope_value = - k * N * (D + 1/M) / M
 asymtope_line = np.linspace(-1, 1, 100) * 1j + asymtope_value
@@ -37,10 +55,10 @@ ax.set_aspect('equal')
 ax.grid(which='both')
 
 ax.text(asymtope_value-0.08, 
-        ax.get_ylim()[1]-0.01, 
+        ax.get_ylim()[0]+0.01, 
         f" {asymtope_value:.3f}", 
             horizontalalignment='center', 
-            verticalalignment='top', 
+            verticalalignment='bottom', 
             color='red')
 
 ax.set_xlabel('Real')
--- IIA/3F1/3F1_FLIGHT_CONTROL/nyquist2.py
+++ IIA/3F1/3F1_FLIGHT_CONTROL/nyquist2.py
@@ -2,6 +2,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+plt.rcParams["text.usetex"] = True
+plt.rcParams["font.family"] = "serif"
+plt.rcParams['figure.facecolor'] = 'white'
+
 # Define the system parameters
 
 ## 2.1 SIMPLIFIED AIRCRAFT MODEL
@@ -11,18 +15,62 @@ W = np.logspace(-10,10, 10000)
 D = 0.250
 T = 0.5
 
-def Gjw(w):
+# G1
+
+def Gjw1(w):
     s = 1j * w
     K = np.exp(- s * D)
     G = 2 / (T*s - 1)
     return G
 
 #polar plot
-G = Gjw(W)
+G = Gjw1(W)
 fig, ax = plt.subplots()
-ax.plot(np.real(G), np.imag(G), color='blue', linewidth=1.0, label='G(s)')
-G = Gjw(-W)
-ax.plot(np.real(G), np.imag(G), color='blue', linewidth=1.0, label='G(s)', linestyle='dashed')
+ax.plot(np.real(G), np.imag(G), color='blue', linewidth=1.0, label='$G_2(s)$')
+
+# arrow
+idx = np.argmin(G.imag)
+x,y = G[idx].real, G[idx].imag
+dx,dy = x - G[idx+1].real, y - G[idx+1].imag
+ax.arrow(x, y, dx, dy, width=0.01, color = 'blue')
+
+G = Gjw1(-W)
+ax.plot(np.real(G), np.imag(G), color='blue', linewidth=1.0, linestyle='dashed')
+
+# -w arrow
+idx = np.argmax(G.imag)
+x,y = G[idx].real, G[idx].imag
+dx,dy = x - G[idx-1].real, y - G[idx-1].imag
+ax.arrow(x, y, dx, dy, width=0.01, color = 'blue')
+
+# G2
+
+def Gjw2(w):
+    s = 1j * w
+    K = np.exp(- s * D)
+    G = K* 2 / (T*s - 1)
+    return G
+
+#polar plot
+G = Gjw2(W)
+ax.plot(np.real(G), np.imag(G), color='orange', linewidth=1.0, label='$K(s)G_2(s)$')
+
+# arrow
+idx = np.argmin(G.imag)
+x,y = G[idx].real, G[idx].imag
+dx,dy = x - G[idx+1].real, y - G[idx+1].imag
+ax.arrow(x, y, dx, dy, width=0.01, color = 'orange')
+
+G = Gjw2(-W)
+ax.plot(np.real(G), np.imag(G), color='orange', linewidth=1.0, linestyle='dashed')
+
+# -w arrow
+idx = np.argmax(G.imag)
+x,y = G[idx].real, G[idx].imag
+dx,dy = x - G[idx-1].real, y - G[idx-1].imag
+ax.arrow(x, y, dx, dy, width=0.01, color = 'orange')
+
+ax.legend()
 
 ax.set_aspect('equal')
 ax.grid(which='both')
