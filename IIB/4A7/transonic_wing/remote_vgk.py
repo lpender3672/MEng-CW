0425838f7318a1df08af1ced2a26caeb35a2d61a
lpender3672 <58957722+lpender3672@users.noreply.github.com>
2024-10-30 23:04:01 +0000

    it works

--- IIB/4A7/transonic_wing/remote_vgk.py
+++ IIB/4A7/transonic_wing/remote_vgk.py
@@ -4,12 +4,12 @@ import os
 import paramiko
 import pyautogui
 import pygetwindow as gw
-import subprocess
 import time
 import re
 from matplotlib import pyplot as plt
+import numpy as np
 
-
+os.environ["QT_AUTO_SCREEN_SCALE_FACTOR"] = "0"
 absp = 'IIB/4A7/transonic_wing/'
 
 with open(absp + '/secrets.json') as f:
@@ -24,11 +24,7 @@ intermediate_hostname = 'gate.eng.cam.ac.uk'
 teaching_hostname = 'ts-access'
 username = "lwp26"
 
-time.sleep(1)
 
-_ = input("Setup Airfoil app to run via Putty and X11 forwarding. Press enter when window is open.")
-
-time.sleep(1)
 
 window = gw.getWindowsWithTitle("Airfoil")[0]
 
@@ -36,6 +32,158 @@ if window:
     window.activate()
     print("Window found")
 
+time.sleep(1)
+
+
+
+class Result():
+    def __init__(self, variables, upper_surface_cp, lower_surface_cp):
+        self.CD2 = variables['CDV+CD2'] - variables['CDV']
+        self.Cd = variables['CDV+CD2']
+        self.Cl = variables['CL']
+        self.Alpha = variables['ALP']
+        self.M = variables['EM']
+
+        self.x_upper, self.cp_upper = zip(*upper_surface_cp)
+        self.x_lower, self.cp_lower = zip(*lower_surface_cp)
+
+class AirfoilApp():
+
+    def __init__(self, ssh_client, window):
+        self.ssh_client = ssh_client
+        #self.ssh_client.exec_command("cd Airfoil-hb")
+
+        self.window = window
+
+        self.M = 0.75
+        self.alpha = 2.0
+
+        x = self.window.left
+        y = self.window.top
+        w = self.window.width
+        h = self.window.height
+
+        self.mach_input_loc = (x + 20, y + h * 0.67)
+        self.alpha_input_loc = (x + w * 0.33 + 20, y + h * 0.67)
+        self.run_loc = (x + w * 0.75, y + h * 0.87)
+
+        
+
+    def run(self, M, alpha):
+
+        if M != self.M:
+            self.M = M
+            self.set_M()
+        
+        if alpha != self.alpha:
+            self.alpha = alpha
+            self.set_alpha()
+
+        self.click_run()
+
+        pyautogui.sleep(2)
+        # sleep
+        # get results
+
+        return self.load_result()
+
+    def set_M(self):
+        # set in window
+        if self.M > 0.95 or self.M < 0.05:
+            raise ValueError("Mach number must be between 0.05 and 0.95")
+        
+        pyautogui.click((
+            self.mach_input_loc
+        ), clicks = 5, interval = 0.1)
+        
+        w = self.window.width
+        dx = (self.M  - 0.05) / (0.95 - 0.05) * w * 0.28
+        
+        pyautogui.click((
+            self.mach_input_loc[0] + dx,
+            self.mach_input_loc[1]
+        ), clicks = 5, interval = 0.1)
+
+    def set_alpha(self):
+        # set in window
+        
+        if self.alpha > 20 or self.alpha < -10:
+            raise ValueError("Angle of attack must be between -20 and 20 degrees")
+        
+        pyautogui.click((
+            self.alpha_input_loc
+        ), clicks = 5, interval = 0.1)
+        
+        w = self.window.width
+        dx = (self.alpha  - -10) / (20 - -10) * w * 0.28
+        
+        pyautogui.click((
+            self.alpha_input_loc[0] + dx,
+            self.alpha_input_loc[1]
+        ), clicks = 5, interval = 0.1)
+
+    def click_run(self):
+
+        pyautogui.click(self.run_loc, clicks=2, interval = 0.05)
+
+    def load_result(self):
+        
+        sftp_client = self.ssh_client.open_sftp()
+        remote_file = sftp_client.open('Airfoil-hb/Autorun.BRF')
+
+        variables = {}
+        upper_surface_cp = []
+        lower_surface_cp = []
+        variable_pattern = re.compile(r"([\w+]+)\s*=\s*([-\d.]+)")
+        cp_data_pattern = re.compile(r"^\s*([\d.]+)\s+([-.\d]+)")
+        parsing_upper = False
+        parsing_lower = False
+
+        # test that result converges
+
+        for line in remote_file:
+
+            # if line empty, skip
+            if not line.strip():
+                continue
+            if "PROCEDURE HAS DIVERGED" in line:
+                return None
+
+            if "Upper surface CP values" in line:
+                parsing_upper = True
+                parsing_lower = False
+                continue
+            elif "Lower surface CP values" in line:
+                parsing_upper = False
+                parsing_lower = True
+                continue
+
+            # Parse variable if not within CP sections
+
+            matches = variable_pattern.findall(line)
+            for var, val in matches:
+                    variables[var] = float(val) if '.' in val else int(val)
+
+            # Parse upper surface CP data
+            if parsing_upper:
+                cp_match = cp_data_pattern.match(line)
+                if cp_match:
+                    x, cp = map(float, cp_match.groups())
+                    upper_surface_cp.append((x, cp))
+
+            # Parse lower surface CP data
+            elif parsing_lower:
+                cp_match = cp_data_pattern.match(line)
+                if cp_match:
+                    x, cp = map(float, cp_match.groups())
+                    lower_surface_cp.append((x, cp))
+
+        out = Result(variables, upper_surface_cp, lower_surface_cp)
+
+        remote_file.close()
+        sftp_client.close()
+
+        return out
 
 
 intermediate_client = paramiko.SSHClient()
@@ -60,145 +208,33 @@ teaching_session = teaching_transport.open_session()
 #teaching_session.request_x11()
 
 
-class Result():
-    def __init__(self, variables, upper_surface_cp, lower_surface_cp):
-        self.CD2 = variables['CDV+CD2'] - variables['CDV']
-        self.Cd = variables['CDV+CD2']
-        self.Cl = variables['CL']
-        self.Alpha = variables['ALP']
-        self.Mach = variables['EM']
-
-        self.x_upper, self.cp_upper = zip(*upper_surface_cp)
-        self.x_lower, self.cp_lower = zip(*lower_surface_cp)
-
-class AirfoilApp():
-
-    def __init__(self, ssh_client, window):
-        self.ssh_client = ssh_client
-        self.ssh_client.exec_command("cd Airfoil-hb")
-
-        self.window = window
-
-        self.M = 0.75
-        self.alpha = 2.0
-
-        x = self.window.left
-        y = self.window.top
-        w = self.window.width
-        h = self.window.height
-
-        self.mach_input_loc = (x + w * 0.33 - 50, y + h * 0.7)
-        self.alpha_input_loc = (x + w * 0.66 - 50, y + h * 0.7)
-        self.run_loc = (x + w * 0.75, y + h * 0.9)
-
-        
-
-    def run(self, M, alpha):
-
-        if M != self.M:
-            self.M = M
-            self.set_M(M)
-        
-        if alpha != self.alpha:
-            self.alpha = alpha
-            self.set_alpha(alpha)
-
-        # run 
-        # sleep
-        # get results
-
-    def set_M(self):
-        # set in window
-        
-        pyautogui.click(self.mach_input_loc, duration=0.5)
-        pyautogui.sleep(0.5)
-        pyautogui.keyDown('backspace')
-        pyautogui.sleep(0.5)
-        pyautogui.keyUp('backspace')
-        pyautogui.write(str(self.M), interval=0.05)
-        pyautogui.sleep(0.5)
-        pyautogui.press('enter')
-        pyautogui.sleep(0.5)
-
-
-    def set_alpha(self):
-        # set in window
-        
-        pyautogui.click(self.alpha_input_loc, duration=0.5)
-        pyautogui.sleep(0.5)
-        pyautogui.keyDown('backspace')
-        pyautogui.sleep(0.5)
-        pyautogui.keyUp('backspace')
-        pyautogui.write(str(self.alpha), interval=0.05)
-        pyautogui.sleep(0.5)
-        pyautogui.press('enter')
-        pyautogui.sleep(0.5)
-
-
-    def load_result(self):
-        
-        sftp_client = self.ssh_client.open_sftp()
-        remote_file = sftp_client.open('Airfoil-hb/Autorun.BRF')
-
-        variables = {}
-        upper_surface_cp = []
-        lower_surface_cp = []
-        variable_pattern = re.compile(r"([\w+]+)\s*=\s*([-\d.]+)")
-        cp_data_pattern = re.compile(r"^\s*([\d.]+)\s+([-.\d]+)")
-        parsing_upper = False
-        parsing_lower = False
-
-        for line in remote_file:
-            if "Upper surface CP values" in line:
-                parsing_upper = True
-                parsing_lower = False
-                continue
-            elif "Lower surface CP values" in line:
-                parsing_upper = False
-                parsing_lower = True
-                continue
-
-            # Parse variable if not within CP sections
-
-            if not (parsing_upper or parsing_lower):
-                matches = variable_pattern.findall(line)
-                for var, val in matches:
-                        variables[var] = float(val) if '.' in val else int(val)
-
-            # Parse upper surface CP data
-            if parsing_upper:
-                cp_match = cp_data_pattern.match(line)
-                if cp_match:
-                    x, cp = map(float, cp_match.groups())
-                    upper_surface_cp.append((x, cp))
-                else:
-                    parsing_upper = False
-
-            # Parse lower surface CP data
-            elif parsing_lower:
-                cp_match = cp_data_pattern.match(line)
-                if cp_match:
-                    x, cp = map(float, cp_match.groups())
-                    lower_surface_cp.append((x, cp))
-                else:
-                    parsing_lower = False
-
-        out = Result(variables, upper_surface_cp, lower_surface_cp)
-
-        remote_file.close()
-        sftp_client.close()
-
-        return out
-
-
 # Parsing flags
 App = AirfoilApp(teaching_client, window)
 
-App.set_M()
-App.set_alpha()
+alphas = np.linspace(0, 3, 5)
+cs = ['r', 'g', 'b', 'y', 'k']
+
+plt.gca().invert_yaxis()
+i = 0
+
+for alpha in alphas:
+    res = App.run(0.75, alpha)
+    if res is None:
+        continue
+
+    plt.plot(res.x_upper, res.cp_upper, label=f"$\\alpha$ = {res.Alpha}, M={res.M}", color=cs[i])
+    plt.plot(res.x_lower, res.cp_lower, color=cs[i])
+
+    i += 1
+
+# set x axis flipped
+
+
+plt.grid()
+plt.legend()
+plt.show()
 
 # Read each line and parse accordingly
-
 
 teaching_client.close()
 intermediate_client.close()
