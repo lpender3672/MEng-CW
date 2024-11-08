6193258265114ba5a458abc38374d360d8858282
lpender3672 <58957722+lpender3672@users.noreply.github.com>
2024-11-08 18:47:19 +0000

    transonic wing progress

--- IIB/4A7/transonic_wing/remote_vgk.py
+++ IIB/4A7/transonic_wing/remote_vgk.py
@@ -18,33 +18,43 @@ class Result():
         self.Cl = variables['CL']
         self.Alpha = variables['ALP']
         self.M = variables['EM']
+        self.Re = 10
+        # currently Re isnt in variables[] from Autorun.BRF, need Autorun.FUL or something else
 
         self.x_upper, self.cp_upper = zip(*upper_surface_cp)
         self.x_lower, self.cp_lower = zip(*lower_surface_cp)
 
 class AirfoilApp():
 
-    def __init__(self, ssh_client, window):
+    def __init__(self, ssh_client, window, foil):
         self.ssh_client = ssh_client
         #self.ssh_client.exec_command("cd Airfoil-hb")
 
         self.window = window
 
+        self.foil = foil
+
         self.M = 0.75
-        self.alpha = 2.0
+        self.alpha = 2.0 # degrees
+        self.Re = 10 # million
 
         x = self.window.left
         y = self.window.top
         w = self.window.width
         h = self.window.height
 
-        self.mach_input_loc = (x + 20, y + h * 0.67)
-        self.alpha_input_loc = (x + w * 0.33 + 20, y + h * 0.67)
-        self.run_loc = (x + w * 0.75, y + h * 0.87)
+        self.mach_input_loc = (x + 80, y + h * 0.776)
+        self.alpha_input_loc = (x + 190, y + h * 0.776)
+        self.run_loc = (x + w * 0.75, y + h * 0.9)
 
         
 
-    def run(self, M, alpha):
+    def run(self, M, alpha, Re):
+
+        if self.check_results_saved(self, M, alpha, Re):
+            return self.load_result
+
+        self.window.activate()
 
         if M != self.M:
             self.M = M
@@ -53,6 +63,10 @@ class AirfoilApp():
         if alpha != self.alpha:
             self.alpha = alpha
             self.set_alpha()
+
+        if Re != self.Re:
+            self.Re = Re
+            self.set_Re()
 
         self.click_run()
 
@@ -69,15 +83,16 @@ class AirfoilApp():
         
         pyautogui.click((
             self.mach_input_loc
-        ), clicks = 5, interval = 0.1)
+        ), duration = 0.1)
         
-        w = self.window.width
-        dx = (self.M  - 0.05) / (0.95 - 0.05) * w * 0.28
-        
-        pyautogui.click((
-            self.mach_input_loc[0] + dx,
-            self.mach_input_loc[1]
-        ), clicks = 5, interval = 0.1)
+        pyautogui.press(
+            "backspace", presses = 7, interval = 0.1
+        )
+
+        pyautogui.typewrite(
+            str(np.round(self.M, 5)),
+            interval = 0.1
+        )
 
     def set_alpha(self):
         # set in window
@@ -87,19 +102,24 @@ class AirfoilApp():
         
         pyautogui.click((
             self.alpha_input_loc
-        ), clicks = 5, interval = 0.1)
+        ),  duration = 0.1)
         
-        w = self.window.width
-        dx = (self.alpha  - -10) / (20 - -10) * w * 0.28
-        
-        pyautogui.click((
-            self.alpha_input_loc[0] + dx,
-            self.alpha_input_loc[1]
-        ), clicks = 5, interval = 0.1)
+        pyautogui.press(
+            "backspace", presses = 8, interval = 0.1
+        )
+
+        pyautogui.typewrite(
+            str(np.round(self.alpha, 5)),
+            interval = 0.1
+            )
+    
+    def set_Re(self):
+        # TODO
+        pass
 
     def click_run(self):
 
-        pyautogui.click(self.run_loc, clicks=2, interval = 0.05)
+        pyautogui.click(self.run_loc, clicks=1, duration=0.1)
 
     def load_result(self):
         
@@ -122,7 +142,8 @@ class AirfoilApp():
             if not line.strip():
                 continue
             if "PROCEDURE HAS DIVERGED" in line:
-                return None
+                out = None
+                break
 
             if "Upper surface CP values" in line:
                 parsing_upper = True
@@ -152,14 +173,58 @@ class AirfoilApp():
                 if cp_match:
                     x, cp = map(float, cp_match.groups())
                     lower_surface_cp.append((x, cp))
+        else:
+            out = Result(variables, upper_surface_cp, lower_surface_cp)
 
-        out = Result(variables, upper_surface_cp, lower_surface_cp)
 
         remote_file.close()
         sftp_client.close()
 
-        return out
+        self.save_result_locally(res)
 
+        return out
+    
+    def save_result_locally(self, res):
+
+        # add data to lookup
+        try:
+            lookup = np.loadtxt(f'data/{self.foil}_lookup.csv', delimiter=',')
+        except FileNotFoundError:
+            lookup = np.empty((0,4))
+        
+        if res is None:
+            # must have been called by a run that immidietly failed
+            # can use self
+            lookup = np.append(lookup, [self.M, self.alpha, self.Re, 0])
+        else:
+            lookup = np.append(lookup, [res.M, res.alpha, res.Re, 1])
+
+        np.savetxt(f'data/{self.foil}_lookup.csv', lookup)
+
+        # save res class using pickle
+        # TODO
+
+    def load_result_locally(self, M, alpha, Re):
+        # returns a result class
+        # will load quite a big file so its best to check lookup before calling this
+        # TODO load pickle file
+        pass
+        
+    def check_result_saved(self, M, alpha, Re):
+        # returns true or false if operating point is in lookup
+
+        try:
+            lookup = np.loadtxt(f'data/{self.foil}_lookup.csv', delimiter=',')
+        except FileNotFoundError:
+            return False
+        
+        for i in range(lookup.shape[0]):
+            if (np.isclose(lookup[i, 0], M) and 
+                np.isclose(lookup[i, 1], alpha) and 
+                np.isclose(lookup[i, 2], Re)):
+                return True
+        
+        return False        
 
 class DPO_Session():
 
@@ -167,7 +232,6 @@ class DPO_Session():
 
         intermediate_hostname = 'gate.eng.cam.ac.uk'
         teaching_hostname = 'ts-access'
-        username = "lwp26"
 
         intermediate_client = paramiko.SSHClient()
         intermediate_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -192,32 +256,40 @@ class DPO_Session():
 
         self.teaching_client = teaching_client
         self.intermediate_client = intermediate_client
+    
+    def end(self):
+        
+        if hasattr(self, 'teaching_client'):
+            self.teaching_client.close()
+        if hasattr(self, 'intermediate_client'):
+            self.intermediate_client.close()
 
-def __del__(self):
-    self.teaching_client.close()
-    self.intermediate_client.close()
 
-# Parsing flags
+def load_password(path):
 
-if __name__ == "__main__":
-
-    try:
-        window = gw.getWindowsWithTitle("Airfoil")[0]
-        window.activate()
-    except IndexError:
-        raise Exception("Window not found")
-
-    time.sleep(1)
-
-    # Setup SSH
-    absp = 'IIB/4A7/transonic_wing/'
-    with open(absp + '/secrets.json') as f:
+    with open(path) as f:
         secrets = json.load(f)
         shifted_pwd = secrets['password']
     pwd = ''
     for s in shifted_pwd:
         pwd += chr(ord(s) - 1)
 
+    return pwd
+
+# Parsing flags
+
+if __name__ == "__main__":
+
+    try:
+        window = gw.getWindowsWithTitle("millenicut:19")[0]
+    except IndexError:
+        raise Exception("Window not found")
+
+    time.sleep(1)
+
+    # Setup SSH
+    absp = 'IIB/4A7/transonic_wing/secrets.json'
+    pwd = load_password(absp)
     sesh = DPO_Session('lwp26', pwd)
 
     # Create the AirfoilApp
@@ -243,5 +315,5 @@ if __name__ == "__main__":
     plt.legend()
     plt.show()
 
-    del sesh
+    sesh.end()
     print("Session closed")
