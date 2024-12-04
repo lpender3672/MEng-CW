fa9c8897e2f02928b3bbc0adbf709586dfdf108c
lpender3672 <58957722+lpender3672@users.noreply.github.com>
2024-12-04 22:10:46 +0000

    started the simple model

---
+++ IIB/4A7/contrail_avoidance/models/simple.py
@@ -0,0 +1,67 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def radiative_forcing(C, C0=600):
+    return 5.35 * np.log(C / C0)
+
+heat_capacity = np.array([1e6, 1e8])
+C_pre = 600 # Gt C
+tau = 200 # years
+k_carbon = 0.5
+
+time_steps = 1000
+dt = 1
+time = np.arange(0, time_steps * dt, dt)
+
+E = np.zeros(time_steps)
+E[100:200] = 30 # Gt C / year for 100 years
+
+C = np.zeros(time_steps)
+C[0] = C_pre
+
+T = np.zeros((time_steps, 2))
+T_base = 288
+T[:, 0] = 288 # atmospheric temperature
+T[:, 1] = 288 # ocean temperature
+
+lambda_sens = 1
+k_mix = 1000
+
+initial_forcing = radiative_forcing(C[0])
+
+for t in range(1, time_steps):
+
+    dC_dt = k_carbon * (E[t] - (C[t - 1] - C_pre) / tau) 
+    C[t] = C[t - 1] + dC_dt * dt
+
+    forcing = radiative_forcing(C[t])
+
+    CdT_dt = np.zeros(2)
+
+    CdT_dt[0] = forcing - lambda_sens * (T[t - 1, 0] - T_base) - k_mix * (T[t - 1, 0] - T[t - 1, 1])
+    CdT_dt[1] = k_mix * (T[t - 1, 0] - T[t - 1, 1])
+
+    T[t] = T[t - 1] + CdT_dt * dt / heat_capacity
+
+ 
+# plot
+
+fig, ax = plt.subplots(1, 1, figsize=(8, 4))
+
+ax.plot(time, T[:, 0], label=f"Atmosphere reservoir")
+ax.plot(time, T[:, 1], label=f"Ocean reservoir")
+
+ax.set_xlabel("Time [years]")
+ax.set_ylabel("Temperature [K]")
+
+plt.legend()
+plt.show()
+
+fig, ax = plt.subplots(1, 1, figsize=(8, 4))
+
+ax.plot(time, C, label="Carbon")
+
+ax.set_xlabel("Time [years]")
+ax.set_ylabel("Carbon [Gt C]")
+
+plt.show()
