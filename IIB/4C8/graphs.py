a001bef09c9f40baf78b051744d8092e8ed167a7
lpender3672 <louispender3672@gmail.com>
2025-03-03 23:53:00 +0000

    started data analysis of 4C8 shouldnt actually take too long

---
+++ IIB/4C8/graphs.py
@@ -0,0 +1,45 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+data52 = [ # W kg, T kg, xi -
+    3, 1, 0.042435424,
+    3, 2, 0.095823096,
+    3, 3, 0.15,
+    5, 1, 0.036199095,
+    5, 2, 0.054973822,
+    5, 3, 0.077452668
+]
+
+W, T, xi = np.array(data52).reshape(-1, 3).T
+
+fig, ax = plt.subplots()
+
+circumference = 3.4 / 12 # 12 v / (v / m) = m
+eff_R = 1e3 * circumference / (2 * np.pi)
+drum_R = 74 / 2 * 1e-3
+g = 9.81
+print(" effective tyre rolling radius R = ", R, "mm")
+
+X = T * g * eff_R / drum_R
+Z = W * g * 1/1 # TODO: measure lever arm ratios
+
+# separate W into 5kg and 3kg
+
+plt.plot(
+    X[W == 3], xi[W == 3], 'o-', label='W = 3kg'
+)
+plt.plot(
+    X[W == 5], xi[W == 5], 'o-', label='W = 5kg'
+)
+
+plt.xlabel('T (kg)')
+plt.ylabel('xi (-)')
+plt.grid()
+plt.legend()
+
+plt.savefig('IIB/4C8/52.png', dpi=300)
+plt.show()
+
+
+
