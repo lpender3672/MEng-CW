36da2210de018b2b8c3803043d2d2beec49b4424
lpender3672 <louispender3672@gmail.com>
2025-03-22 10:02:05 +0000

    have been unable to structure the weight matrix as feedback im actually going to power through 4A4 today instead

--- IIB/4F2/oscillatory_NNs_and_pharmocokinetics/Q1i.m
+++ IIB/4F2/oscillatory_NNs_and_pharmocokinetics/Q1i.m
@@ -1,17 +1,47 @@
 
-A = [-10, 0; 0, -100];
-B = [10; 100];
-C = [1, 0; 0, 1; 0, 0];
-D = [0; 0; 1];
+As = [-10, 0; 0, -100];
+Bs = [10; 100];
+Cs = [1, 0; 0, 1; 0, 0];
+Ds = [0; 0; 1];
 
-Gsubs = ss(A, B, C, D);
+Gsubs = ss(As, Bs, Cs, Ds);
 s = tf('s');
 
 G = Gsubs * 1/(s+1);
 
+G
 
 w1 = 1.0;
-w2 = 2.0;
-w3 = 0.5;
+w2 = 1.0;
+w3 = 1.0;
+
+A0 =[-1, 0, 0; 10, -10, 0; 100, 0, -100];
+N = [w3, w1, w2; zeros(2,3)];
+B = [1; 0; 0];
+C = [1, 0, 0];
+
 
 simulate_nonlinear_system(w1, w2, w3);
+
+n = size(A0, 1)
+m = size(B, 2)
+
+cvx_begin sdp
+    variable Y(n,n) symmetric
+    variable Z(m,n)
+    variable epsilon
+    variable gama
+    
+    %minimise(gama)
+    minimise(epsilon) % otherwise makes very high
+
+    LMI1 = Y >= 0
+    LMI2 = gama <= 0.1
+    
+    % gain LMI
+    %
+    LMI3 = [ (Y*A' + A*Y + Z'*Bu' + Bu*Z),   (Y*Cz' + Z'*Dzu'),          Bw;
+            (Cz*Y + Dzu*Z),  -gama*eye(2),      Dzw';
+            Bw',            Dzw,                -gama*eye(2) ] <= 0;
+    
+cvx_end
